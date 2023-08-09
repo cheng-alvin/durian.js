0346919620e2bde522674414689fa6c069928fef
Alvin Cheng <88267875+cheng-alvin@users.noreply.github.com>
2023-08-09 19:51:05 +1000

    Update webpack.config.js

--- packages/sample/webpack.config.js
+++ packages/sample/webpack.config.js
@@ -1,11 +1,11 @@
-// eslint-disable-next-line
-// @ts-nocheck `webpack.config.js` is a config file and does not require checking. 
-import path from "path";
+// @ts-nocheck `webpack.config.js` is a config file and does not require checking.
+const path = require("path");
 
 module.exports = {
+  mode: "development", // Note:  Set to `development` for testing, please use `production` for production environments.
   entry: "./src/index.js",
   output: {
     path: path.resolve(__dirname, "public/"),
-    filename: "app.js"
+    filename: "app.js",
   },
 };
