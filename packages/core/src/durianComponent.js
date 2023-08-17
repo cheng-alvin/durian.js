73753f1c183c3def2342f81ff2592a8e032266ce
Alvin Cheng <88267875+cheng-alvin@users.noreply.github.com>
2023-08-17 18:31:44 +1000

    Rearranged code

--- packages/core/src/durianComponent.js
+++ packages/core/src/durianComponent.js
@@ -5,18 +5,28 @@ export class DurianComponent extends HTMLElement {
   async connectedCallback() {
     await this.sleep(10);
     this.style = "display: none;";
-    const name = this.testComponentNaming(this.getAttribute("name"));
+
+    const name = this.getAttribute("name");
+    this.validateComponentAttributes();
+
     const componentHTML = this.innerHTML;
     customElements.define(name, componentFactory(componentHTML));
   }
 
-  testComponentNaming(name) {
-    if (!name.includes("-"))
+  validateComponentAttributes() {
+    const name = this.getAttribute("name");
+
+    if (!name) {
+      throw new Error(
+        "The `name` attribute is required in ALL durian components! Try finding a name-less component in your html!",
+      );
+    }
+
+    if (!name.includes("-")) {
       throw new SyntaxError(
         `Component names must include a dash \`-\`! Try adding a dash to: \`${name}\`!`,
       );
-
-    return name;
+    }
   }
 
   sleep(ms) {
