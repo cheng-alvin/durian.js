670c31ae97663408391047c1ac61a933a01c8e12
Alvin Cheng <88267875+cheng-alvin@users.noreply.github.com>
2023-08-19 12:08:20 +1000

    Added JSDoc as mentioned in review

--- packages/core/src/index.js
+++ packages/core/src/index.js
@@ -1,5 +1,20 @@
 import { DurianComponent } from "./durianComponent.js";
 
+/**
+ * Initializes the durian framework. If imported as a module,
+ * otherwise, it will be initialized automatically when imported as a `<script>`.
+ *
+ * @example
+ * import __durian__ from "@durian/core";
+ * __durian__();
+ *
+ * // or
+ *
+ * require("@durian/core")();
+ *
+ * @returns {void}
+ */
+
 export default function __durian__() {
   customElements.get("durian-component") ||
     customElements.define("durian-component", DurianComponent);
