9ce204c6bfaa649b34a8b71eb3f7e42f93140c5d
Alvin Cheng <88267875+cheng-alvin@users.noreply.github.com>
2023-08-20 22:03:34 +1000

    Corrected docs

--- packages/core/src/index.js
+++ packages/core/src/index.js
@@ -1,8 +1,8 @@
 import { DurianComponent } from "./durianComponent.js";
 
 /**
- * Initializes the durian framework. If imported as a module,
- * otherwise, it will be initialized automatically when imported as a `<script>`.
+ * Initializes the durian framework and checks basic browser compatibility when imported with a module format.
+ * Otherwise, it will be initialized automatically when imported as a `<script>`.
  *
  * @example
  * import __durian__ from "@durian/core";
@@ -15,7 +15,31 @@ import { DurianComponent } from "./durianComponent.js";
  * @returns {void}
  */
 
-export default function __durian__() {
-  customElements.get("durian-component") ||
-    customElements.define("durian-component", DurianComponent);
+export default function __durian__(bypassBrowserCheck = false) {
+  if (window.durianExecuted) return;
+
+  if (!bypassBrowserCheck) {
+    if (typeof window === "undefined")
+      throw new Error(
+        "Durian can only be used in a browser environment! Try using a bundler like `webpack` or`parcel`! We cannot find the `window` object present inside a browser's Javascript environment, make sure Javascript is allowed and enabled in your browser!",
+      );
+
+    if (!window.customElements)
+      throw new Error(
+        "Durian requires the `customElements` API to be present in the browser. Please download and link a polyfill package like `@webcomponents/custom-elements` or `@webcomponents/webcomponentsjs`!",
+      );
+
+    if (!window.ShadowRoot)
+      throw new Error(
+        "Durian requires the `ShadowRoot` API to be present in the browser. Please download and link a polyfill package like `@webcomponents/shadydom` or `@webcomponents/webcomponentsjs`!",
+      );
+  } else {
+    console.warn(
+      "Bypassing browser checks, could be problematic later on if not setup correctly!",
+    );
+  }
+
+  customElements.define("durian-component", DurianComponent);
+  customElements.define("durian-prop", DurianProp);
+  window.durianExecuted = true;
 }
