91a71e58a0b9607722d8079b1372302ce4d2be32
Alvin Cheng <88267875+cheng-alvin@users.noreply.github.com>
2023-08-20 22:00:04 +1000

    Added browser checks

--- packages/core/src/index.js
+++ packages/core/src/index.js
@@ -31,12 +31,28 @@ import { DurianProp } from "./durianProp.js";
  */
 
 export default function __durian__(bypassBrowserCheck = false) {
-  if (typeof window === "undefined")
-    throw new Error(
-      "Durian can only be used in a browser environment! Try using a bundler like `webpack` or`parcel`! We cannot find the `window` object present inside a browser's Javascript environment, make sure Javascript is allowed and enabled in your browser!",
-    );
-
   if (window.durianExecuted) return;
+  
+  if (!bypassBrowserCheck) {
+    if (typeof window === "undefined")
+      throw new Error(
+        "Durian can only be used in a browser environment! Try using a bundler like `webpack` or`parcel`! We cannot find the `window` object present inside a browser's Javascript environment, make sure Javascript is allowed and enabled in your browser!",
+      );
+
+    if (!window.customElements)
+      throw new Error(
+        "Durian requires the `customElements` API to be present in the browser. Please download and link a polyfill package like `@webcomponents/custom-elements` or `@webcomponents/webcomponentsjs`!",
+      );
+
+    if (!window.ShadowRoot)
+      throw new Error(
+        "Durian requires the `ShadowRoot` API to be present in the browser. Please download and link a polyfill package like `@webcomponents/shadydom` or `@webcomponents/webcomponentsjs`!",
+      );
+  } else {
+    console.warn(
+      "Bypassing browser checks, could be problematic later on if not setup correctly!",
+    );
+  }
 
   customElements.define("durian-component", DurianComponent);
   customElements.define("durian-prop", DurianProp);
