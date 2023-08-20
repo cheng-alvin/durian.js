b92372ea473c5dc152712e388cefe1da3262e3b2
Alvin Cheng <88267875+cheng-alvin@users.noreply.github.com>
2023-08-20 15:28:07 +1000

    Encapsulated the durian logic
    Here, I've just encapsulated the basic primitive durian logic and the very simple loading into a separate `DurianPrimitive` class to prevent repeating

---
+++ packages/core/src/durianPrimitive.js
@@ -0,0 +1,11 @@
+export class DurianPrimitive extends HTMLElement {
+  async connectedCallback() {
+    await this.sleep(10);
+    this.style = "display: none;";
+    this?.main();
+  }
+
+  sleep(ms) {
+    return new Promise((resolve) => setTimeout(resolve, ms));
+  }
+}
