3ee02f32aaee7692ba80b0a8bf18543b784eb5c0
Alvin Cheng <88267875+cheng-alvin@users.noreply.github.com>
2023-08-20 16:28:35 +1000

    Added a browser check and annotated with jsdoc
    I've annotated the browser check with jsdoc as well as added the auto-call statement here as mentioned in e02067800071562245ca9bd7e219f6a4121388c0

--- packages/core/src/index.js
+++ packages/core/src/index.js
@@ -1,8 +1,14 @@
 import { DurianComponent } from "./durianComponent.js";
+import { DurianProp } from "./durianProp.js";
 
 /**
  * Initializes the durian framework. If imported as a module,
  * otherwise, it will be initialized automatically when imported as a `<script>`.
+ *
+ * @author cheng-alvin
+ *
+ * @param {boolean} [bypassBrowserCheck=false] - Bypasses the browser `window` checks.
+ * @note Browser checks would always run when imported as a `<script>`.
  *
  * @example
  * import __durian__ from "@durian/core";
@@ -12,10 +18,29 @@ import { DurianComponent } from "./durianComponent.js";
  *
  * require("@durian/core")();
  *
+ * @example
+ * // Without browser checks
+ * import __durian__ from "@durian/core";
+ * __durian__(true);
+ *
+ * // or
+ *
+ * require("@durian/core")(true);
+ *
  * @returns {void}
  */
 
-export default function __durian__() {
-  customElements.get("durian-component") ||
-    customElements.define("durian-component", DurianComponent);
+export default function __durian__(bypassBrowserCheck = false) {
+  if (typeof window === "undefined")
+    throw new Error(
+      "Durian can only be used in a browser environment! Try using a bundler like `webpack` or`parcel`! We cannot find the `window` object present inside a browser's Javascript environment, make sure Javascript is allowed and enabled in your browser!",
+    );
+
+  if (window.durianExecuted) return;
+
+  customElements.define("durian-component", DurianComponent);
+  customElements.define("durian-prop", DurianProp);
+  window.durianExecuted = true;
 }
+
+__durian__();
