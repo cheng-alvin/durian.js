9b188d5bdf64b18e61ff3d47936226f85e7889f8
Alvin Cheng <88267875+cheng-alvin@users.noreply.github.com>
2023-09-01 11:51:26 +1000

    Added deprecation

--- packages/core/src/shadowRootEval.js
+++ packages/core/src/shadowRootEval.js
@@ -1,3 +1,7 @@
+/**
+ * @deprecated
+ */
+
 export function shadowRootEval($) {
   $.querySelectorAll("script").forEach((script) => {
     eval(script.innerHTML); // Potential fix? - Alvin <eventide1029@gmail.com>
