248d2e4398ca79d8d5200319be90cbe531ab4629
Alvin Cheng <88267875+cheng-alvin@users.noreply.github.com>
2023-09-01 11:52:55 +1000

    Added warning

--- packages/core/src/shadowRootEval.js
+++ packages/core/src/shadowRootEval.js
@@ -6,6 +6,7 @@ export function shadowRootEval($) {
   console.warn(
     "`shadowRootEval()` using the `<script>` is deprecated, support for `<script>` will be removed in the future!",
   );
+
   $.querySelectorAll("script").forEach((script) => {
     eval(script.innerHTML); // Potential fix? - Alvin <eventide1029@gmail.com>
   });
