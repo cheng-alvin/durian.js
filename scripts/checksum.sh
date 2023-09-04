f448ec404432b09044629bf07a24b2dee26e1333
Alvin Cheng <88267875+cheng-alvin@users.noreply.github.com>
2023-09-04 16:51:20 +1000

    Revert "Removed verbose code"
    This reverts commit 8d74a4fc63b587548be5c9f0718773dfc915baec.

--- scripts/checksum.sh
+++ scripts/checksum.sh
@@ -1,20 +1,26 @@
+#!/bin/bash
 tar -czvf temp.tar.gz $(git ls-files -o --exclude-standard)
 
+# Check if the file 'temp.tar.gz' exists
 if [ ! -f "temp.tar.gz" ]; then
   echo "File 'temp.tar.gz' does not exist. Exiting."
   exit 1
 fi
 
+# Calculate the SHA-256 checksum for the 'temp.tar.gz' file
 actual_checksum=$(shasum -a 256 temp.tar.gz | awk '{print $1}')
 
+# Ask the user for the expected checksum
 echo "Enter the expected SHA-256 checksum:"
 read expected_checksum
 
+# Check if the input is empty
 if [ -z "$expected_checksum" ]; then
   echo "Input is empty. Exiting."
   exit 1
 fi
 
+# Compare the user-provided checksum with the actual checksum
 if [ "$expected_checksum" = "$actual_checksum" ]; then
   echo "Checksums match. File 'Durian.js' is valid."
   rm -r temp.tar.gz
