1ae2942932a6b71abde1ca8ad328763799890b8c
Alvin Cheng <88267875+cheng-alvin@users.noreply.github.com>
2023-09-07 22:37:00 +1000

    POTENTIAL SOLUTION FOUND! 🎉

--- packages/core/src/component.js
+++ packages/core/src/component.js
@@ -4,6 +4,18 @@ export function componentFactory(innerHTML) {
   innerHTML = innerHTML.replaceAll("&lt;", "<").replaceAll("&gt;", ">");
 
   return class Component extends HTMLElement {
+    connectedCallback() {
+      // Solution? - Alvin
+      const scriptElements = this.shadowRoot.querySelectorAll("script");
+
+      scriptElements.forEach((script) => {
+        const newScript = document.createElement("script");
+        newScript.textContent = script.textContent;
+        this.shadowRoot.appendChild(newScript);
+        script.remove();
+      });
+    }
+
     constructor() {
       super();
       this.attachShadow({ mode: "open" });
