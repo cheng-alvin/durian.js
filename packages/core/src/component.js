28dde1733be353d202df63284377e27f76688c2d
Alvin Cheng <88267875+cheng-alvin@users.noreply.github.com>
2023-09-09 06:21:38 +1000

    Fixed review issues 1&2
    Fixed:
    
    *Probably useless, can delete
    *Since scriptElements are just used once, I can probably just conbine it with line 11
    
    As outlined in #20

--- packages/core/src/component.js
+++ packages/core/src/component.js
@@ -1,14 +1,10 @@
-// import { shadowRootEval } from "./shadowRootEval";
-
 export function componentFactory(innerHTML) {
   innerHTML = innerHTML.replaceAll("&lt;", "<").replaceAll("&gt;", ">");
 
   return class Component extends HTMLElement {
     connectedCallback() {
       // Solution? - Alvin
-      const scriptElements = this.shadowRoot.querySelectorAll("script");
-
-      scriptElements.forEach((script) => {
+      this.shadowRoot.querySelectorAll("script").forEach((script) => {
         const newScript = document.createElement("script");
         newScript.textContent = script.textContent;
         this.shadowRoot.appendChild(newScript);
